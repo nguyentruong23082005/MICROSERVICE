2932c2c38523fa21a8b6d80b00ff19a83f765bcb
tructruong2308 <nguyentruong23082005@gmail.com>
2026-06-08 22:40:54 +0700

    chore: untrack and ignore giaodienmau.md and init.sql

--- init.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE users;
-CREATE DATABASE product_catalog;
-CREATE DATABASE orders;
-CREATE DATABASE product_recommendations;
-CREATE DATABASE payments;
-CREATE DATABASE notifications;
